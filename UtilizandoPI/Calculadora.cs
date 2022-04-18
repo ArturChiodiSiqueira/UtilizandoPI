630801b0e1b01c4b529ed557848b9de479f912a9
ArturChiodiSiqueira <arturchiodi@gmail.com>
2022-04-18 15:24:14 -0300

    Add project files.

---
+++ UtilizandoPI/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoPI
+{
+    internal class Calculadora
+    {
+        public static double Diametro(double raio)
+        {
+            return 2.0 * raio;
+        }
+
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * Math.PI * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return (4 / 3.0) * Math.PI * Math.Pow(raio, 3.0);
+        }
+    }
+}
---
+++ UtilizandoPI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UtilizandoPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume;
+ 
+            Console.Write("ENTRE COM O RAIO (r): ");
+            raio = double.Parse(Console.ReadLine());
+
+            if (raio < 0)
+            {
+                Console.WriteLine("\nINFORME UM RAIO POSITIVO!!");
+            }
+            else
+            {
+                double diametro = Calculadora.Diametro(raio);
+                Console.WriteLine("\nO diametro é: " + diametro.ToString("F3"));
+
+                double circunferencia = Calculadora.Circunferencia(raio);
+                Console.WriteLine("A circunferência é: " + circunferencia.ToString("F3"));
+
+                volume = Calculadora.Volume(raio);
+                Console.WriteLine("O volume é: " + volume.ToString("F3"));
+            }
+            Console.WriteLine("\n\nFIM DO PROGRAMA!");
+            Console.ReadKey();
+        }
+    }
+}
